84eab2fcd07b92d6d911c7c3d255d79919be61be
Anthrax <marx.simon@gmx.de>
2018-12-01 13:24:30 +0100

    Minimized code

--- Assets/_Goo/Scripts/CubeMovingTest.cs
+++ Assets/_Goo/Scripts/CubeMovingTest.cs
@@ -5,20 +5,14 @@ using UnityEngine;
 public class CubeMovingTest : MonoBehaviour
 {
     public new Camera camera;
-    public GameObject Cube;
+    public GameObject Cube;  //Attachen zu Blobbls
 
-    public float speed = 10f;
     public Vector3 targetPos;
-    public bool isMoving;
-    const int MOUSE = 0;
-
-
 
 
     void Start()
     {
         targetPos = transform.position;
-        isMoving = false;
     }
 
     void Update()
@@ -26,35 +20,13 @@ public class CubeMovingTest : MonoBehaviour
         RaycastHit hit;
         Ray ray = camera.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(ray, out hit))
-        {
-            Transform objectHit = hit.transform;
+        if (Physics.Raycast(ray, out hit) && Input.GetMouseButton(0))
 
-            if (Input.GetMouseButtonUp(0))
-            {
-                isMoving = false;
-            }
-            if (Input.GetMouseButton(0))
-            {
-                Debug.Log("Hit");
-                isMoving = true;
-           
-
-            }
-            
-        }
-        if (isMoving)
         {
             targetPos = camera.ScreenToWorldPoint(Input.mousePosition, 0);
-            Debug.Log("Release");
-            MoveObject();
+            transform.position = targetPos;
+
         }
-    }
 
-
-    void MoveObject()
-    {
-
-        transform.position = targetPos;
     }
 }
